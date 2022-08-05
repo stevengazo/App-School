b80da8bd73a0aafc2b013135f92c54e46da3a0b7
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-08-04 21:05:26 -0600

    refactoring control

--- controller/control.php
+++ controller/control.php
@@ -7,60 +7,19 @@
     require_once "connections/conexion.php";
     require_once "Model/Nota.php";
 
-    /**
-     * Controladores
-     * Cuando "control" es invocado, este recibe un controlador y le 
-     * envia la acción a este.
-     */
-    
-    require_once "controller/AdministradorController.php";
-    require_once "controller/AlumnoController.php";
-    require_once "controller/AsignaturaController.php";    
-    require_once "controller/AsignaturaHasAlumnoController.php";
-    require_once "controller/FaltaAsistenciaController.php";
-    require_once "controller/HorariosController.php";    
-    require_once "controller/NivelController.php";
-    require_once "controller/NotasController.php";
-    require_once "controller/ProfesorController.php";
 
-    /**
-     * Controladores
-     */
-    require_once 'FaltaAsistenciaController.php';
     class control{
         /**
          * vARIABLES INTERNAS DE LA CLASE
          */
         private $Smarty;
 
-        private $AdministradorController;
-        private $AlumnoController;
-        private $AsignaturaController;
-        private $AsignaturaHasAlumnoController;
-        private $FaltaAsistenciaController;
-        private $HorariosController;
-        private $NivelController;
-        private $NotasController;
-        private $ProfesorController;
-
-        private $FaltaAsistencia;
-        private $Nota;
         
-        
-    
-
-
         /**
          * Funcion constructora
          */
         function __construct(){
             $this->Smarty= new config_smarty();
-            $this->FaltaAsistencia = Falta_Asistencia::getInstancia();
-            $this->Nota = Nota::getInstancia();    
-            $this->NotasController = NotasController::getInstancia(); 
-            $this->FaltaAsistenciaController = FaltaAsistenciaController::getInstancia();       
-            $this->AsignaturaHasAlumnoController = AsignaturaHasAlumnoController::getInstancia();
-
         }
 
         /**
@@ -94,26 +53,14 @@
         * Descripción: Gestiona las peticiones del navegador, recibe un parametro controller y redirecciona al controlador especificado
         */
         function gestor(){       
-            $Controller= "";                               
+
             $action = "";        
             if(isset($_REQUEST['action'])){ // Compruea que la variable "action" no sea nula
                 $action = $_REQUEST['action']; // si no es nula setea la variable acciòn con el valor recibido
             }
-            if(isset($_REQUEST['Controller'])){ // Compruea que la variable "action" no sea nula
-                $Controller = $_REQUEST['Controller']; // si no es nula setea la variable acciòn con el valor recibido
-            }
-            switch ($Controller) {
-                case 'Notas':
-                    $this->NotasController->Gestor($action);
-                    break;   
-                    case 'Profesor':
-                        $this->ProfesorController->Gestor($action);
-                        break;                       
-                case 'FaltaAsistencia':
-                        $this->FaltaAsistenciaController->Gestor($action);
-                        break;                                    
-                case 'AsignaturaHasAlumno':
-                    $this->AsignaturaHasAlumnoController->Gestor($action);
+            switch ($action) {      
+                case 'index':
+                    $this->index();
                     break;                                                            
                 default:     
                     $this->index();
